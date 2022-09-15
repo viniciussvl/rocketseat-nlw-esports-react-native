4bbd9a2ef986f8949789deae669c55853b598d19
Vinicius Aquino <viniciussvl@hotmail.com>
2022-09-14 21:28:41 -0300

    list games on Home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,19 @@
+import { useEffect, useState } from "react";
 import { View, Image, FlatList } from 'react-native';
-
+import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from "../../components/GameCard";
-
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
 
-import { GAMES } from '../../utils/games';
-import { styles } from './styles';
-
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://192.168.0.114:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data))
+  }, [])
+
   return (
     <View style={styles.container}>
       <Image
@@ -22,7 +27,7 @@ export function Home() {
       />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard
